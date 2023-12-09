ff813b342585083fcb62304a7036d76da1c2d9b5
Martim <Martim@GONCERMOROSPRO>
2023-12-09 07:21:06 -0800

    Add project files.

---
+++ Goncermor Launcher/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static Goncermor_Launcher.WIN32.DWM_API;
+
+namespace Goncermor_Launcher
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            IntPtr hWnd = new WindowInteropHelper(GetWindow(this)).EnsureHandle();
+            DWM_WINDOW_CORNER_PREFERENCE Pref = DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_DONOTROUND;
+            WIN32.DWM_API.DwmSetWindowAttribute(hWnd, DWMWINDOWATTRIBUTE.DWMWA_WINDOW_CORNER_PREFERENCE, ref Pref, sizeof(uint));
+        }
+    }
+}
---
+++ Updater/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Updater
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e) => _ = AsyncTask();
+
+        private async Task AsyncTask()
+        {
+            Storyboard? Sb_Spin = this.FindResource("Spin") as Storyboard;
+            Storyboard? Sb_Show = this.FindResource("Show") as Storyboard;
+            Sb_Show?.Begin();
+            await Task.Delay(1900);
+            Container.Opacity = 1;
+            ContainerScale.ScaleX = 1;
+            ContainerScale.ScaleY = 1;
+            ColorWheelScale.ScaleX = 1;
+            ColorWheelScale.ScaleY = 1;
+            ColorWheelBlurScale.ScaleX = 1;
+            ColorWheelBlurScale.ScaleY = 1;
+            Sb_Spin?.Begin();
+            
+        }
+    }
+}
