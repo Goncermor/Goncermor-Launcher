94f4d0c9dcaed8496d4241d10b297236fac96161
Goncermor <goncermor@gmail.com>
2024-01-03 06:57:58 -0800

    Designed Mod List Item

---
+++ Goncermor Launcher/Controls/ModListItem.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Goncermor_Launcher.Controls
+{
+    public partial class ModListItem : UserControl
+    {
+        public ModListItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
