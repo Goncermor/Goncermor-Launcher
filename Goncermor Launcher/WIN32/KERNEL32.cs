6107009c2df5180f5cb548fafec30f5a23e9095c
Goncermor <goncermor@gmail.com>
2024-02-18 04:36:37 -0800

    Upload of WIn32 Methods

---
+++ Goncermor Launcher/WIN32/KERNEL32.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static Goncermor_Launcher.MainWindow;
+
+namespace Goncermor_Launcher.WIN32
+{
+    class KERNEL32
+    {
+        [DllImport("kernel32.dll")]
+        public static extern int AllocConsole();
+    }
+}
---
+++ Goncermor Launcher/WIN32/USER32.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static Goncermor_Launcher.MainWindow;
+
+namespace Goncermor_Launcher.WIN32
+{
+    class USER32
+    {
+        [DllImport("user32.dll")]
+        public static extern int SetWindowText(IntPtr hWnd, string text);
+    }
+}
